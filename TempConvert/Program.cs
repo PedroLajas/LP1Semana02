053f22cadbb3e2f8538270039af15b47e37d96b9
Pedro Lajas <pedrolajas.gamedesigner@gmail.com>
2026-02-26 16:55:39 +0000

    Ex. 3

--- TempConvert/Program.cs
+++ TempConvert/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Microsoft.VisualBasic;
 
 namespace TempConvert
 {
@@ -7,9 +8,25 @@ namespace TempConvert
         private static void Main(string[] args)
         {
             Console.WriteLine("Write down a temperature number: ");
-            string temperature = Console.ReadLine();
+            double temperature = double.Parse(Console.ReadLine());
             Console.WriteLine("C or F? ");
             string unit = Console.ReadLine();
-        }
+            
+            double converted;
+
+            switch(unit)
+            {
+                case "C":
+                    converted = temperature * 1.8 + 32;
+                    Console.WriteLine($"{temperature:F2} C = {converted:F2} F");
+                    Console.WriteLine($"Absolute value: {Math.Abs(converted):F2}");
+                    break;
+                case "F":
+                    converted = (temperature - 32) / 1.8;
+                    Console.WriteLine($"{temperature:F2} F = {converted:F2} C");
+                    Console.WriteLine($"Absolute value: {Math.Abs(converted):F2}");
+                    break;
+            }
     }
+}
 }
