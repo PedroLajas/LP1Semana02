fb9ed2c20847f904f98ebb355f7568dd22c616b1
Pedro Lajas <pedrolajas.gamedesigner@gmail.com>
2026-02-26 16:20:12 +0000

    Ex. 2

---
+++ TempConvert/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TempConvert
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Write down a temperature number: ");
+            string temperature = Console.ReadLine();
+            Console.WriteLine("C or F? ");
+            string unit = Console.ReadLine();
+        }
+    }
+}
